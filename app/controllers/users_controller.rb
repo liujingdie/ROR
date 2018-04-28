1e9700604b801d77ebc79949883fa35532340870
JD Liu <liujingdie@gmail.com>
2018-04-28 14:18:52 +0000

    Finish user edit, update, index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  def index
+    @users = User.all
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -8,7 +13,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)    # 不是最终的实现方式
+    @user = User.new(user_params)    
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -19,10 +24,45 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 处理更新成功的情况
+    else
+      render 'edit'
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
 end
